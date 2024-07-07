7326d891006c184f824202abd40c30f7baa191d0
kytrancsinh <dangkytran123@gmail.com>
2024-07-07 21:03:13 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -1,10 +1,10 @@
 const CONFIGDATA = {
-  titleHeader: "Chào cậu, tớ có điều muốn nói. </br> Làm người yêu tớ nhé!",
+  titleHeader: "Chào cậu, tớ có điều muốn nói. </br> Cậu có thấy A8 2024 tuyệt vời ko?",
   descriptionHeader:
     "Hãy trả lời một cách thật lòng nhé. Nếu cậu im lặng và rời đi thì tức là cậu đã ĐỒNG Ý.",
-  buttonYes: "Tớ đồng ý",
-  buttonNo: "Không đồng ý",
-  titleModar: "Cảm ơn cậu.",
+  buttonYes: "Có tuyệt vời là có tuyệt vời",
+  buttonNo: "Không lớp xàm vcho",
+  titleModar: "Đúng vậy, it's a fact",
   descriptionModar:
-    "Cảm ơn cậu đã cho tớ một cơ hội để tớ có thể ở bên cạnh, che chở và bảo vệ cậu. </br> Tớ biết tớ không được đẹp trai, cũng chẳng lắm tài nhưng tớ có một trái tim chân thành. Cậu chính là cả thế giới của tớ. </br> Tối nay tớ sẽ đến đón cậu. Cậu mặc thật đẹp vào nhé! Sẽ có một bất ngờ dành cho cậu.</br> Love you <3",
+    "A8 à, suốt những năm qua tớ đã rất vui, có lúc ko, nhưng chung quy là tớ rất yêu quí lớp. A8 luôn là điều tớ vênh mặt lên khi ra sân trường (tao 9A8 đó mày) , tự hào khoe với cả TDN rằng mình là thành viên của A8 2024!!. </br> T biết 4 năm là khá dài, nma vẫn chưa đủ đối với t, chắc mốt học trường mới lớp mới, t phải lấy kỉ yếu ra xem cho đỡ nhớ quá. </br> Hi bữa nào họp lớp phải đi đủ đó nha, t luỵ tdn 1 nma luỵ những con người của A8 10.</br> Love you all 9A8 <333",
 };
